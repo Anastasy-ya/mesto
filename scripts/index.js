b15a0b8afd5402a0fd47dc00805f6861632c2014
Hiperiosity <anastasy_ya@mail.ru>
2022-12-10 20:34:40 +0300

    javascripting

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
+/*profile__edit-button кнопка редактирования
+popup_opened доп класс
+popup__button отправить
+popup__close-icon
+popup
+*/
+
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close-icon');
+
+editButton.addEventListener('click', function(event) {
+  console.log(event);
+  event.preventDefault();
+  popup.classList.add('popup_opened');
+})
+
+
 
