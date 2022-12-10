d91db3005fe410779b29e3a85e1dc2479aeb2ea4
Hiperiosity <anastasy_ya@mail.ru>
2022-12-10 22:42:05 +0300

    javascripting

--- scripts/index.js
+++ scripts/index.js
@@ -9,11 +9,20 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close-icon');
 
-editButton.addEventListener('click', function(event) {
-  console.log(event);
+editButton.addEventListener('click', openPopup);
+popup__close-icon.addEventListener('click', closePopup);
+
+  function openPopup(event) {
   event.preventDefault();
   popup.classList.add('popup_opened');
-})
+}
 
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 
-
+popup.addEventListener('click', function(ClickBackground) {
+  if(ClickBackground.target === ClickBackground.currentTarget) {
+    closePopup();
+  }
+});
