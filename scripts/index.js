c59d2365075d7f455130ae57841cd218c89769b1
Hiperiosity <anastasy_ya@mail.ru>
2023-01-16 15:13:08 +0300

    карточки создаются но не так

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
 //задали переменные для редактирования имени и профессии
-let editButton = document.querySelector(".profile__edit-button");
-let popup = document.querySelector(".popup");
-let popupClose = document.querySelectorAll(".popup__close-icon");
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelectorAll('.popup__close-icon');
 
 // console.log(popupClose)
 
 
-let nameInput = document.querySelector('[name="Name"]');
-let jobInput = document.querySelector('[name="About"]');
+const nameInput = document.querySelector('[name="Name"]');
+const jobInput = document.querySelector('[name="About"]');
 
 
-let formElement = document.querySelector(".popup__form");
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__about");
-
-
-
+const formElement = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__about');
 
 
 
-let editPopup = document.querySelector(".popup_edit");
+
+
+
+const editPopup = document.querySelector('.popup_edit');
 
 
 
@@ -30,7 +30,7 @@ let editPopup = document.querySelector(".popup_edit");
 
 //функция открытия попапов
 function openPopup(popup) {
-    popup.classList.add("popup_opened");
+    popup.classList.add('popup_opened');
 
 }
 
@@ -43,7 +43,7 @@ function addNameAndJob() {
 
 //функция закрытия попапа
 function closePopup(popup) {
-    popup.classList.remove("popup_opened");
+    popup.classList.remove('popup_opened');
 }
 
 //функция сохранения
@@ -56,10 +56,10 @@ function handleFormSubmit(evt) {
 }
 
 //cоздали слушатели событий по кнопке открытия и закрытия для редактирования профиля
-editButton.addEventListener("click", addNameAndJob);
-// popupClose.addEventListener("click", closePopup);
+editButton.addEventListener('click', addNameAndJob);
+// popupClose.addEventListener('click', closePopup);
 //навесим слушатель событий на submit формы
-formElement.addEventListener("submit", handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit);
 
 
 //
@@ -73,7 +73,7 @@ formElement.addEventListener("submit", handleFormSubmit);
 popupClose.forEach(button => {
   button.addEventListener('click', function (event) {
       // console.log(event.currentTarget.closest(".popup"));
-      closePopup(event.currentTarget.closest(".popup"));
+      closePopup(event.currentTarget.closest('.popup'));
 
     });
 });
@@ -95,30 +95,30 @@ popupClose.forEach(button => {
 
 
 //задали переменные для редактирования добавления карточек  кажется надо переименовать перем и задавать через конст
-let addButton = document.querySelector(".add-button");
-let addPopup = document.querySelector(".popup_add");
+let addButton = document.querySelector('.add-button');
+let addPopup = document.querySelector('.popup_add');
 
 //задали переменные для увеличения картинки
-const bigImage = document.querySelector(".popup_image");
-const imageToOpen = document.querySelectorAll(".elements__image");
+const bigImage = document.querySelector('.popup_image');
+const imageToOpen = document.querySelectorAll('.elements__image');
 
 //cоздали слушатели событий по кнопке добавления карточек
 //и открытия попапа для редактирования профиля
-addButton.addEventListener("click", () => {
+addButton.addEventListener('click', () => {
 openPopup(addPopup);
 });
 
 
 //ф-я открытия попапа для увеличения картинки
 imageToOpen.forEach(button => {
-button.addEventListener("click", () => {
+button.addEventListener('click', () => {
 openPopup(bigImage);
 }
 )});
 
 
 //задали переменные для лайков
-const like = document.querySelectorAll(".button-like");
+const like = document.querySelectorAll('.button-like');
 
 
 
@@ -126,7 +126,7 @@ const like = document.querySelectorAll(".button-like");
 like.forEach(button => {
   button.addEventListener('click', function (event) {
     const eventTarget = event.target;
-    eventTarget.classList.toggle("button-like_active");
+    eventTarget.classList.toggle('button-like_active');
     });
 });
 
@@ -165,86 +165,147 @@ const initialCards = [
 ];
 
 
-// let itemName = //initialCards.сюда сложить массив из name
-// let itemImage =
+//ф-я передающая данные из массива в ф-ю создания карточки
+createStartItems = array => {
+  array.forEach(({itemName, itemImage}) => {
+  createItem(itemName, itemImage);
 
-
-
-
-//ф-я в которую попадает название задачи
-const createItem = (itemName, itemImage) => {
-//объявим переменные
-const template = document.querySelector('#template');//темплейт откуда копируем
-console.log(template);
-// клонируем содержимое тега template
-const templateElement = template.content.querySelector('.elements__item').cloneNode(true);
-
-//тут не определился либо темплейт либо его содержимое ПАЧАМУ????
-
-
-
-//const elementsItem = template.querySelector('.elements__item');//карточка
-//const elementsImage = template.querySelector('.elements__image');//картинка
-//const elementsSignature = template.querySelector('.elements__signature');//подпись
-
-
-
-
-
-// наполняем содержимым заменить функцией которая переберет массивы
-templateElement.querySelector('.elements__signature').textContent = itemName;//меняем подпись   itemName
-
-//templateElement.querySelector('.elements__image').src = itemImage;//ой не факт что это сработает
-return templateElement;
+  })
 };
 
+createStartItems(initialCards);
+//console.log(createStartItems);
 
+//ф-я создания карточки
+function createItem (itemName, itemImage) {
+const template = document.querySelector('#template').content;//содержимое темплейта
+const templateElement = template.querySelector('.elements__item').cloneNode(true);//li копируем
+//меняем содержимое полей
+templateElement.querySelector('.elements__signature').textContent = itemName;
+templateElement.querySelector('.elements__image').src = itemImage;
+templateElement.querySelector('.elements__image').alt = itemName;//может быть понадобится `${}`
+console.log(templateElement);
+return templateElement;
 
+};
 
-
-
-
-
-//ф-я вставки темплейта в elements__box
+// ф-я вставки темплейта в elements__box
+addItems = element => {
 const elementsBox = document.querySelector('.elements__box');//общая коробка для вставки карточек
+elementsBox.prepend(element);
+};
 
-const addItem = (itemName, itemImage) => {
-elementsBox.prepend(templateElement);
-}
-//вызов ф-и
-addItem('Архыз', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg');
-
-// // наполняем содержимым код из теории
-// userElement.querySelector('.elements__signature').src = 'tinyurl.com/v4pfzwy';
-// userElement.querySelector('.user__name').textContent = 'Дюк Корморант';
-
-// // отображаем на странице
-// usersOnline.append(userElement);
+addItems(createStartItems(initialCards));
 
 
+// renderCard(createItem(itemName, itemImage), elementsBox)
+
+// const elementsBox = document.querySelector('.elements__box');
+
+// function renderCard(element, containerElement) {
+// containerElement.prepend(element);
+// }
 
 
-// мой кривой код с онлайн кодинга
-// //ф-я добаувления итема
-// const createItem = (ItemName, itemImage) => {
 
-// //ф-я клонирования и определения содержимого
-// const Item = template.querySelector('.elements__item').cloneNode(true);//клонируем карточки
-// Item.querySelector('.elements__signature').textContent = itemName;//меняем подпись
+// function sendFormAddCard(evt) {
 
-// //Item.querySelector('.elements__image')    =itemImage    меняем картинку ;
-// //Item.querySelector('.elements__image')    = itemName;   меняем alt для картинки ;
-// return createItem;
+//   evt.preventDefault();
+//   const name = nameInputCard.value;
+//   const link = linkInputCard.value;
+//   createCard(name, link);
+//   closePopup(cardsPopup)
+// }
 
+
+
+
+
+
+
+
+//ниже дубликат кода
+
+// // let itemName = //initialCards.сюда сложить массив из name
+// // let itemImage =
+
+
+
+
+// //ф-я в которую попадает название задачи
+// const createItem = (itemName, itemImage) => {
+// //объявим переменные
+// const template = document.querySelector('#template');//темплейт откуда копируем
+// console.log(template);
+// // клонируем содержимое тега template
+// const templateElement = template.content.querySelector('.elements__item').cloneNode(true);
+
+// //тут не определился либо темплейт либо его содержимое ПАЧАМУ????
+
+
+
+// //const elementsItem = template.querySelector('.elements__item');//карточка
+// //const elementsImage = template.querySelector('.elements__image');//картинка
+// //const elementsSignature = template.querySelector('.elements__signature');//подпись
+
+
+
+
+
+// // наполняем содержимым заменить функцией которая переберет массивы
+// templateElement.querySelector('.elements__signature').textContent = itemName;//меняем подпись   itemName
+
+// //templateElement.querySelector('.elements__image').src = itemImage;//ой не факт что это сработает
+// return templateElement;
 // };
 
-// //добавим в dom наш итем
-// const makeItem = (itemName, itemImage) => {
-//   elementsBox.apend(createItem(ItemName,  itemImage));
 
 
-// };
-// makeItem('Байкал', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg');
-// console.log(makeItem);
 
-// //console.log(elementsBox, elementsItem, elementsImage, elementsSignature);  itemName
+
+
+
+
+// //ф-я вставки темплейта в elements__box
+// const elementsBox = document.querySelector('.elements__box');//общая коробка для вставки карточек
+
+// const addItem = (itemName, itemImage) => {
+// elementsBox.prepend(templateElement);
+// }
+// //вызов ф-и
+// addItem('Архыз', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg');
+
+// // // наполняем содержимым код из теории
+// // userElement.querySelector('.elements__signature').src = 'tinyurl.com/v4pfzwy';
+// // userElement.querySelector('.user__name').textContent = 'Дюк Корморант';
+
+// // // отображаем на странице
+// // usersOnline.append(userElement);
+
+
+
+
+// // мой кривой код с онлайн кодинга
+// // //ф-я добаувления итема
+// // const createItem = (ItemName, itemImage) => {
+
+// // //ф-я клонирования и определения содержимого
+// // const Item = template.querySelector('.elements__item').cloneNode(true);//клонируем карточки
+// // Item.querySelector('.elements__signature').textContent = itemName;//меняем подпись
+
+// // //Item.querySelector('.elements__image')    =itemImage    меняем картинку ;
+// // //Item.querySelector('.elements__image')    = itemName;   меняем alt для картинки ;
+// // return createItem;
+
+// // };
+
+// // //добавим в dom наш итем
+// // const makeItem = (itemName, itemImage) => {
+// //   elementsBox.apend(createItem(ItemName,  itemImage));
+
+
+// // };
+// // makeItem('Байкал', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg');
+// // console.log(makeItem);
+
+// // //console.log(elementsBox, elementsItem, elementsImage, elementsSignature);  itemName
