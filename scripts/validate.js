ea8f1d9967730cab807a033d55726f11daadda72
Hiperiosity <anastasy_ya@mail.ru>
2023-02-01 21:29:43 +0300

    hasInvalidInput не работает

---
+++ scripts/validate.js
@@ -0,0 +1,121 @@
+// // включение валидации вызовом enableValidation
+// // все настройки передаются при вызове
+
+// enableValidation({
+//   formSelector: '.popup__form',
+//   inputSelector: '.popup__input',
+//   submitButtonSelector: '.popup__button',
+//   inactiveButtonClass: 'popup__button_disabled',
+//   inputErrorClass: 'popup__input_type_error',
+//   errorClass: 'popup__error_visible'
+// });
+
+
+//showInputError
+//hideInputError
+//inputList-массив инпутов
+//inputSelector-один инпут
+//formList массив форм
+// formSelector форма
+// submitButtonSelector кнопка в форме
+
+// form__input_type_error
+// form__input-error_active
+
+
+
+
+// показать сообщение об ошибке тут разобраться с классами
+const showInputError = (formSelector, inputSelector, errorMessage) => {//форма инпут и inputSelector.validationMessage
+  //errorMessage не найдена
+  const inputErrorClass = formSelector.querySelector(`.${inputSelector.id}-error`);//не определена
+  inputSelector.classList.add('form__input_type_error');
+  inputErrorClass.textContent = errorMessage;
+  inputErrorClass.classList.add('form__input-error_active');
+};
+
+// скрыть сообщение об ошибке тут разобраться с классами
+const hideInputError = (formSelector, inputSelector) => {
+  const inputErrorClass = formSelector.querySelector(`.${inputSelector.id}-error`);//не определена
+  inputSelector.classList.remove('form__input_type_error');
+  inputErrorClass.textContent = errorMessage;
+  inputErrorClass.classList.remove('form__input-error_active');
+};
+
+// проверить валидны ли поля инпутов
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputSelector) => {
+    console.log(inputSelector.validity);//
+    return !inputSelector.validity.valid;//ошибка тут, поля не валидны примерно всегда, ПОЧЕМУ????
+  });//false если не валидны
+};
+
+//показать/скрыть сообщение об ошибке
+const checkInputValidity = (formSelector, inputSelector) => {
+  if (!inputSelector.validity.valid) {//если форма валидна
+    showInputError(formSelector, inputSelector, inputSelector.validationMessage);//показать
+  } else {
+    hideInputError(formSelector, inputSelector);//если нет скрыть
+  }
+};
+
+
+//button_inactive
+//проверить валидны ли поля и переключить вид кнопки   submitButtonSelector попадает из внешнего окружения
+const toggleButtonState = (inputList, submitButtonSelector) => {//в ф-ю попадет массив инпутов для проверки их состояния и кнопка сабмит
+  if (hasInvalidInput(inputList)) {//если поля невалидны    по ходу  эта функция косячит
+    submitButtonSelector.classList.add('popup__button_inactive');//не фиг тыкать тут        кнопка сабмит покрасилась в нерабочий вид, но обратно в рабочий вид не красится
+  } else {//если нет
+    submitButtonSelector.classList.remove('popup__button_inactive');//покрасить кнопочку так, чтобы хотелось по ней ткнуть
+  }
+};//ok кроме вставленной ф-и
+
+
+//ф-я, которая навесит слушатели событий полям ввода и кнопке
+const setEventListeners = (formSelector) => {//возьмем фОРМУ
+  const inputList = Array.from(formSelector.querySelectorAll('.popup__dates'));//найдем массив инпутов
+  //ok
+  const submitButtonSelector = formSelector.querySelector('.popup__button');//найдем кнопку сабмит, это по ходу тоже массив?????
+  //ok
+  //console.log(submitButtonSelector);
+  toggleButtonState(inputList, submitButtonSelector);//получим данные о валидности и переключим состояние кнопки сабмит
+  inputList.forEach((inputSelector) => {//для каждого элемента inputElement из массива инпутов inputSelector
+
+    inputSelector.addEventListener('input', function () {//навесим слушатель   ЕГО НЕТ В КОНСОЛИ, НО СТИЛИ ПОМЕНЯЛИСЬ, ЗНАЧИТ РАБОТАЕТ
+
+      checkInputValidity(formSelector, inputSelector);//
+      toggleButtonState(inputList, submitButtonSelector);//в случае надобности изменим состояние кнопки сабмит
+
+    });
+  });
+};//ok
+
+
+
+//навесить слушатели всем полям и отменить дефолтное действие
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));//получить массив из форм         почему снова ищем формы?
+//ok
+  formList.forEach((formSelector) => {//для каждого элемента formSelector массива formList
+    //formElement.setAttribute('novalidate', '');
+    //debugger;
+
+    // formSelector.addEventListener('submit', function (evt) {//по сабмиту отменить отправку на сервер
+    //   evt.preventDefault();
+    // }); // кажется эта деталь лишняя
+
+    // const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
+    // fieldsetList.forEach((fieldSet) => {
+
+     setEventListeners(formSelector);//выполнить ф-ю, которая навесит слушатели событий полям ввода и кнопке
+    // })
+
+  });
+};//ok
+
+enableValidation(); //вызовем, навесив слушатели
+
+
+
+
+//debugger;
